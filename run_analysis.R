5e014b8b838c6b41915385a33ce0150d63c57695
MaryKate <kate.academia@traxtech.com>
2015-02-22 18:38:51 -0700

    Minor revision

--- run_analysis.R
+++ run_analysis.R
@@ -5,13 +5,13 @@ library(plyr)
 # Merge the training and test sets to create one data set
 ###############################################################################
 
-x_train <- read.table("train/X_train.txt")
-y_train <- read.table("train/y_train.txt")
-subject_train <- read.table("train/subject_train.txt")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
-x_test <- read.table("test/X_test.txt")
-y_test <- read.table("test/y_test.txt")
-subject_test <- read.table("test/subject_test.txt")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
 # create 'x' data set
 x_data <- rbind(x_train, x_test)
@@ -26,7 +26,7 @@ subject_data <- rbind(subject_train, subject_test)
 # Extract only the measurements on the mean and standard deviation for each measurement
 ###############################################################################
 
-features <- read.table("features.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
 
 # get only columns with mean() or std() in their names
 mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
@@ -41,7 +41,7 @@ names(x_data) <- features[mean_and_std_features, 2]
 # Use descriptive activity names to name the activities in the data set
 ###############################################################################
 
-activities <- read.table("activity_labels.txt")
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 # update values with correct activity names
 y_data[, 1] <- activities[y_data[, 1], 2]
